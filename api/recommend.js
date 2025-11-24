9ab9506df07b6d91631ebfbc15f1aca717a4f471
Kartikp160 <kartikpal264@gmail.com>
2025-11-24 10:41:00 +0530

    CORS error fix

--- api/recommend.js
+++ api/recommend.js
@@ -1,21 +1,23 @@
-import fetch from "node-fetch";
-
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
-    return res.status(405).json({ error: "Only POST allowed" });
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
   }
 
   try {
-    const backend = await fetch("http://35.223.25.198:8000/recommend", {
+    const backendRes = await fetch("http://35.223.25.198:8000/recommend", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body)
     });
 
-    const data = await backend.json();
-    return res.status(200).json(data);
-
+    const data = await backendRes.json();
+    res.status(200).json(data);
   } catch (err) {
-    return res.status(500).json({ error: err.toString() });
+    res.status(500).json({ error: err.toString() });
   }
 }
